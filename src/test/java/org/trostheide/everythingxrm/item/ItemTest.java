0e577ddaa18418208b59b4a5249d102f12b932e4
utrost <uwe.trostheide@gmail.com>
2022-06-27 16:18:23 +0200

    test for missing name when saving item entity record

--- src/test/java/org/trostheide/everythingxrm/item/ItemTest.java
+++ src/test/java/org/trostheide/everythingxrm/item/ItemTest.java
@@ -6,6 +6,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import javax.validation.ConstraintViolation;
+import javax.validation.Validator;
+import javax.validation.groups.Default;
+
+import java.util.Set;
+
 import static org.assertj.core.api.Assertions.assertThat;
 @SpringBootTest
 class ItemIntegrationTest {
@@ -15,6 +21,9 @@ class ItemIntegrationTest {
 
     @Autowired
     SystemAuthenticator systemAuthenticator;
+
+    @Autowired
+    Validator validator;
 
     @Test
     void given_valid_item_when_saveItem_then_itemIsSaved() {
@@ -30,6 +39,24 @@ class ItemIntegrationTest {
 
         //then
         assertThat(savedItem.getId()).isNotNull();
+
+    }
+
+    @Test
+    void given_itemWithMissingName_when_validateItem_then_itemIsInvalid() {
+
+        // given
+        Item item = dataManager.create(Item.class);
+
+        // item name is mandatory. therefore to test if its working i'll set it to null. ValidationViolation shall be caught then
+        item.setName(null);
+        item.setDescription("This is a Description");
+
+        //when
+        Set<ConstraintViolation<Item>> violations = validator.validate(item, Default.class);
+
+        //then
+        assertThat(violations).hasSize(1);
 
     }
 
