0be90597b0adbc9bdb8de8a2e0c275321c4670ef
utrost <uwe.trostheide@gmail.com>
2022-07-20 18:26:23 +0200

    entity log on record level for items

--- src/main/java/org/trostheide/everythingxrm/item/screen/ItemEdit.java
+++ src/main/java/org/trostheide/everythingxrm/item/screen/ItemEdit.java
@@ -1,11 +1,15 @@
 package org.trostheide.everythingxrm.item.screen;
 
+import io.jmix.audit.entity.EntityLogItem;
 import io.jmix.ui.UiComponents;
 import io.jmix.ui.action.BaseAction;
 import io.jmix.ui.component.Component;
 import io.jmix.ui.component.LinkButton;
 import io.jmix.ui.component.Table;
 import io.jmix.ui.download.Downloader;
+import io.jmix.ui.model.CollectionLoader;
+import io.jmix.ui.model.InstanceContainer;
+import io.jmix.ui.model.InstanceLoader;
 import io.jmix.ui.screen.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.trostheide.everythingxrm.item.Item;
@@ -35,6 +39,22 @@ public class ItemEdit extends StandardEditor<Item> {
         } else {
             return new Table.PlainTextCell("<empty>");
         }
+    }
+
+    @Autowired
+    private InstanceLoader<Item> itemDl;
+    @Autowired
+    private CollectionLoader<EntityLogItem> entityLogItemsDl;
+
+    @Subscribe
+    public void onBeforeShow(BeforeShowEvent event) {
+        itemDl.load();
+    }
+
+    @Subscribe(id = "itemDc", target = Target.DATA_CONTAINER)
+    public void onItemDcItemChange(InstanceContainer.ItemChangeEvent<Item> event) {
+        entityLogItemsDl.setParameter("item", event.getItem().getId());
+        entityLogItemsDl.load();
     }
 
 }
