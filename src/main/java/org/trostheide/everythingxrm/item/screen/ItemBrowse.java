00be68d8cb2b2c4ef8971e97103cc6ea908b2005
utrost <46228819+utrost@users.noreply.github.com>
2022-07-21 18:00:08 +0200

    Pagination

--- src/main/java/org/trostheide/everythingxrm/item/screen/ItemBrowse.java
+++ src/main/java/org/trostheide/everythingxrm/item/screen/ItemBrowse.java
@@ -1,10 +1,14 @@
 package org.trostheide.everythingxrm.item.screen;
 
+
 import io.jmix.ui.screen.*;
 import org.trostheide.everythingxrm.item.Item;
+
 
 @UiController("xrm_Item.browse")
 @UiDescriptor("item-browse.xml")
 @LookupComponent("itemsTable")
 public class ItemBrowse extends StandardLookup<Item> {
+
+
 }
